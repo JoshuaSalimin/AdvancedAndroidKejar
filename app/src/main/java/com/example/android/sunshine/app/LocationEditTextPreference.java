35b3a160e6785c2c86a954ad8406266ac631d8da
joshua.salimin <13514001@std.stei.itb.ac.id>
2017-04-22 21:16:57 +0700

    add positive button

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -4,10 +4,17 @@ package com.example.android.sunshine.app;
  * Created by Joshua on 4/22/2017.
  */
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LocationEditTextPreference extends EditTextPreference {
     static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
@@ -24,5 +31,37 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
+    }
+
+    @Override
+    protected void showDialog(Bundle state){
+        super.showDialog(state);
+        EditText et = getEditText();
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog d = getDialog();
+                if (d instanceof AlertDialog){
+                    AlertDialog dialog = (AlertDialog) d;
+                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    if(s.length()< mMinLength){
+                        positiveButton.setEnabled(false);
+                    }
+                    else{
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
     }
 }
