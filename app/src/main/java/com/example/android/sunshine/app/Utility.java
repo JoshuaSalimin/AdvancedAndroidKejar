2449ed10913c3f9a5714c529b9088739224677b3
joshua.salimin <13514001@std.stei.itb.ac.id>
2017-04-22 20:56:01 +0700

    condition for invalid location

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -263,4 +263,11 @@ public class Utility {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
         return sp.getInt(c.getString(R.string.pref_location_status_key), SunshineSyncAdapter.LOCATION_STATUS_SERVER_UNKNOWN);
     }
+
+    public static void resetLocationStatus(Context c){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_location_status_key), SunshineSyncAdapter.LOCATION_STATUS_SERVER_UNKNOWN);
+        spe.apply();
+    }
 }
